ba42ba3645c4bbbe6fa5cc608b7e9e6a0468384f
wkmikw <wkmikw@hotmail.com>
2017-04-19 19:42:30 +0800

    t1

---
+++ t1.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+
+'''
+def h():
+    print ('Wen Chuan'),
+    m = yield 5  # Fighting!
+    print(m)
+    d = yield 12
+    print ('We are together!')
+c = h()
+next(c)  #相当于c.send(None)
+c.send('Fighting!')  #(yield 5)表达式被赋予了'Fighting!'
+
+
+
+def consumer():
+    r = ''
+    while True:
+        n = yield r
+        if not n:
+            return
+        print('[CONSUMER] Consuming %s...' % n)
+        r = '200 OK'
+
+def produce(c):
+    c.send(None)
+    n = 0
+    while n < 5:
+        n = n + 1
+        print('[PRODUCER] Producing %s...' % n)
+        r = c.send(n)
+        print('[PRODUCER] Consumer return: %s' % r)
+    c.close()
+
+c = consumer()
+produce(c)
+'''
+
+# 导入:
+from sqlalchemy import Column, String, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+# 创建对象的基类:
+Base = declarative_base()
+
+# 定义User对象:
+class User(Base):
+    # 表的名字:
+    __tablename__ = 'user'
+
+    # 表的结构:
+    id = Column(String(20), primary_key=True)
+    name = Column(String(20))
+
+# 初始化数据库连接:
+engine = create_engine('mysql+pymysql://root:www3111197@localhost:3306/test')
+# 创建DBSession类型:
+DBSession = sessionmaker(bind=engine)
+
+# 创建session对象:
+session = DBSession()
+# 创建新User对象:
+new_user = User(id='7', name='Bob')
+# 添加到session:
+session.add(new_user)
+# 提交即保存到数据库:
+session.commit()
+# 关闭session:
+session.close()
+
+
+# 创建Session:
+session = DBSession()
+# 创建Query查询，filter是where条件，最后调用one()返回唯一行，如果调用all()则返回所有行:
+user = session.query(User).filter(User.id=='5').one()
+# 打印类型和对象的name属性:
+print('type:', type(user))
+print('name:', user.name)
+# 关闭Session:
+session.close()
+
+# python t1.py
